565b7a49383d8e772e343c3271753e98e1712ca0
Ashstolemybike <75098246+Ashstolemybike@users.noreply.github.com>
2022-07-28 19:11:36 +0100

    added testing for getting status code

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,1 +1,11 @@
+import json
+import requests
 
+
+url = requests.get("https://swapi.dev/")
+print(url)
+
+class GetApi:
+
+    def get_api(self):
+        pass
--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,23 @@
 # Unit Testing Plan
+import unittest
+import sys
+
+import requests
+
+sys.path.insert(0,"..")
+from ..app.requesting_sw import *
 
 # Test function that pulls all available starships from api
+
+class Apitests(unittest.TestCase):
+
+    api_test = GetApi()
+
+    def test_get_api(self):
+        url = requests.get("https://swapi.dev/")
+        self.assertEqual(url.status_code,200)
+
+
+
+
+
