b9b1fb9651dbfb0cf56f1511ba4b9c7adc52620c
C1nth14 <cynthiasusan31@gmail.com>
2022-07-29 13:51:17 +0100

    Testing the extracting API function

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,11 @@
 # Unit Testing Plan
+import requests
+
 
 # Test function that pulls all available starships from api
+from starwars.app.requesting_sw import *
+
+
+def test_get_api():
+
+    assert get_api().status_code == 200
