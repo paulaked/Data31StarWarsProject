a37696c43c5b644b3050175feaa8f25624558da5
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-29 17:50:37 +0100

    Basic function for status code of the API (sample function to check if API is responding)

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,1 +1,25 @@
+import json
+import requests
+import pymongo
+
+
+class GetStarwarsAPI:
+
+    def get_data_api(self):
+        '''
+        function to test if API is responding
+        :return: the status code of the API
+        '''
+        status_code = requests.get("https://swapi.dev/")
+        return status_code
+
+
+
+
+
+
+
+
+
+
 
