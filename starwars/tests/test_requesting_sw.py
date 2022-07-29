faf970fc93eef4c7bae962d18bf451efaae87fd4
Bilal <bilalullah@hotmail.com>
2022-07-29 18:27:14 +0100

    first test added - needs fixing

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,11 @@
 # Unit Testing Plan
 
 # Test function that pulls all available starships from api
+
+import unittest
+
+# Test function that pulls all available starships from api
+
+class ApiTesting(unittest.TestCase):
+
+    api_test = get_api('https://swapi.dev/')
