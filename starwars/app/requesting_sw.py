340d9c876b9603eb059eb06586017cc88b13c5ae
Bilal <bilalullah@hotmail.com>
2022-07-29 16:51:21 +0100

    API function and starship database function added

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,1 +1,20 @@
+import pymongo
+import requests
 
+
+def get_api(url):  # get API
+    response = requests.get(url)
+    return response
+
+
+def db_link(name):  # Connect to database 'starwars'
+    client = pymongo.MongoClient()
+    db = client[name]
+
+    try:  # check if collection exists
+        db.create_collection("starships")
+    except:
+        print("Starship collection already exists")
+        db.starships.drop()
+        db.create_collection("starships")
+    return db
