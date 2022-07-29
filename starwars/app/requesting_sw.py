e507b644b8b68533229dabe0ef48d4265e237a5b
Ashstolemybike <75098246+Ashstolemybike@users.noreply.github.com>
2022-07-29 10:08:36 +0100

    minor tweaks

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -2,10 +2,10 @@ import json
 import requests
 
 
-url = requests.get("https://swapi.dev/")
-print(url)
+#url = requests.get("https://swapi.dev/")
 
 class GetApi:
 
     def get_api(self):
-        pass
+        status_code = requests.get("https://swapi.dev/")
+        return status_code
