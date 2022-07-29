39fbffb72b0c688de88f03ef5c1b6942613bb98e
C1nth14 <cynthiasusan31@gmail.com>
2022-07-29 18:07:37 +0100

    git commit -m "Structure for testing in the works."

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,11 +1,21 @@
 # Unit Testing Plan
-import requests
 
-
-# Test function that pulls all available starships from api
 from starwars.app.requesting_sw import *
+# Test function that pulls all available starships from api
 
 
 def test_get_api():
 
     assert get_api().status_code == 200
+
+def test_get_api_info():
+
+    pass
+
+def test_get_starships():
+
+    pass
+
+def test_drop_unwanted_columns():
+
+    pass
