9f3d28ea4e615f0d9782c7da84946e4e4dcda3f5
C1nth14 <cynthiasusan31@gmail.com>
2022-07-29 13:56:31 +0100

    Extracting function is added

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,1 +1,19 @@
+import requests
+import json
 
+#starships = requests.get("https://swapi.dev/api/starships/")
+#print(starships)
+#print(starships.json())
+
+
+def get_api():
+    url = "https://swapi.dev/api/starships/"
+    request_api = requests.get(url)
+    resp_json = request_api.json()
+
+    return request_api
+
+
+starship = get_api()
+
+print(starship)
