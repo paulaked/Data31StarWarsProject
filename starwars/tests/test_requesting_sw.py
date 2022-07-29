765fb5d7f311bddc6f5397a5b39cac6e876733e2
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-29 17:48:48 +0100

    Test function to check status code (sample test to check if API is responding)

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,19 @@
 # Unit Testing Plan
-
 # Test function that pulls all available starships from api
+import unittest
+import sys
+
+sys.path.insert(0,"..")
+from ..app.requesting_sw import *
+
+
+class APIUnittests(unittest.TestCase):
+
+    api_testing = GetStarwarsAPI()
+
+    def test_get_api(self):
+        '''
+        Check to see if API is responding
+        :return: checks if the status code matches the provided value
+        '''
+        self.assertEqual(self.api_testing.get_data_api().status_code, 200)
