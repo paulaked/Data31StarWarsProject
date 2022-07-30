35fcb0b98e1b5a905e1954be346e49d2dc5f92ef
gabensaw <gabriel.sawicki1995@gmail.com>
2022-07-30 18:41:32 +0100

    adding test function test_get_all_sratships

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -5,7 +5,11 @@ import json
 
 def api_connection(url):
     headers = {"Content-Type": "application/json"}
-
     response = requests.get(url, headers=headers)
     return response.json()
+
+
+def get_all_starships(api_dict):
+    pass
+
 
--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -2,13 +2,19 @@ import requests
 import pytest
 from starwars.app.requesting_sw import *
 
+url = "http://swapi.dev/api/starships"
+
 
 def test_connection_status_code():
-    url = "http://swapi.dev/api/starships"
     response = requests.get(url)
     assert response.status_code == 200
 
 
 def test_api_connection():
-    assert isinstance(api_connection("http://swapi.dev/api/starships"), dict)
+    assert isinstance(api_connection(url), dict)
 
+
+def test_get_all_starships():
+    response_json = requests.get(url).json()
+    all_starships = response_json['count']
+    assert len(get_all_starships(api_connection(url))) == all_starships
