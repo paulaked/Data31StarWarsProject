245bc33eaf276159bf5bea7f11ad3d2dbefe175b
gabensaw <gabriel.sawicki1995@gmail.com>
2022-07-30 18:22:39 +0100

    rename test_api_connection function to test_connection_status_code, add new test_api_connection function and update api_connection requestig function

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -3,13 +3,9 @@ import requests
 import json
 
 
-def api_connection():
-    url = "http://swapi.dev/api/starships"
-    headers = {'Content-Type': 'application/json'}
-    json_body = json.dumps(["starships"])
+def api_connection(url):
+    headers = {"Content-Type": "application/json"}
 
-    response = requests.get(url, headers=headers, data=json_body)
+    response = requests.get(url, headers=headers)
     return response.json()
 
-
-print(api_connection())
--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,10 +1,14 @@
 import requests
 import pytest
+from starwars.app.requesting_sw import *
 
 
-def test_api_connection():
+def test_connection_status_code():
     url = "http://swapi.dev/api/starships"
     response = requests.get(url)
     assert response.status_code == 200
 
+
+def test_api_connection():
+    assert isinstance(api_connection("http://swapi.dev/api/starships"), dict)
 
