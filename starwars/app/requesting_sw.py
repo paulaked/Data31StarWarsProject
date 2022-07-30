34544f6748d2661aabd12077f21779a03f1e5dea
gabensaw <gabriel.sawicki1995@gmail.com>
2022-07-30 18:50:12 +0100

    adding function clean_all_starships which remove not needed informations

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -5,6 +5,7 @@ import json
 
 def api_connection(url):
     headers = {"Content-Type": "application/json"}
+
     response = requests.get(url, headers=headers)
     return response.json()
 
@@ -21,10 +22,22 @@ def get_all_starships(api_dict):
     return results_list
 
 
+def clean_all_starships(starships_list):
+    for item in starships_list:
+        item.pop("created")
+        item.pop("edited")
+        item.pop("url")
+    return starships_list
+
+
 url = "http://swapi.dev/api/starships"
 
 connection_dict = api_connection(url)
 ships_list = get_all_starships(connection_dict)
 
-print(connection_dict)
 print(ships_list)
+
+clean_ships_list = clean_all_starships(ships_list)
+
+print(clean_ships_list[0])
+
