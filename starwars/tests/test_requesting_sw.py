05ad66f04341d5763f682eee45c9150ab9700b65
Adenike Akinyemi <adenike80@hotmail.com>
2022-07-31 13:49:55 +0100

    added 2 tests

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,19 @@
-# Unit Testing Plan
+from main import *
+import unittest
 
-# Test function that pulls all available starships from api
+
+class Unit_tests(unittest.TestCase):
+
+    def test_api_request(self):
+        try:
+            request_api("https://swapi.dev/api/starships")
+        except AssertionError:
+            self.fail("api_request: An exception occurred")
+
+    def test_character_id_search(self):
+        try:
+            character_id_search("Luke Skywalker")
+        except AssertionError:
+            self.fail("character_id_search: An exception occurred")
+
+    
