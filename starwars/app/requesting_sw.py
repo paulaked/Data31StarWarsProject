2c45adbca842cba219739b685afe6604f85f4283
Savan Meggi <you@example.comsmeggi@spartaglobal.comgit config --global user.name Your>
2022-08-01 10:48:24 +0100

    Created first test but cant get it to find the correct .py file

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,9 +1,8 @@
 import requests
 import json
 
+# Calling the API
 json_body = {"Content-Type": "application/json"}
-
 starships_req = requests.get("http://swapi.dev/api/starships", headers=json_body)
-
 print(starships_req.json())
 
--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,8 @@
-# Unit Testing Plan
-
+import requests
+import pytest
+from starwars.app.requesting_sw import*
 # Test function that pulls all available starships from api
+
+def test_conection_status_code():
+    response = requests.get("http://swapi.dev/api/starships")
+    assert response.status_code == 200
