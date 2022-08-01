544c08807e2ac55f7e79a818aeb55573eab489ee
AliShaheed <alishaheed1138@gmail.com>
2022-08-01 10:45:24 +0100

    added test for response

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,32 @@
 # Unit Testing Plan
+"""
+classes
+	Starships - to call starships list
+		test response code
+		test isinstance json
+	StarshipsModel
+		check returns only desired fields - mock entry
 
+function:
+	extract
+		calls url
+		function to store starships as dictionary
+			check isValid dictionary ('Death Star')
+"""
 # Test function that pulls all available starships from api
+import unittest
+import json 
+from starship import Starships
+from model import StarshipsModel
+from main import *
+
+
+class StarshipsTest(unittest.TestCase):
+    starships = Starships()
+
+    def test_response(self):
+        self.assertEquals(self.starships(""), 200)
+        self.assertEquals(self.starships(""), 404)
+    
+    def  test_json(self):
+        self.assertIsInstance(self.starships(""), json)
