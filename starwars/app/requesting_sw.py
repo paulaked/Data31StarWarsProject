1b109bb5f0a31a98dad7f3d5ce5d3661f1833f82
anaghieh <109526313+anaghieh@users.noreply.github.com>
2022-08-01 22:40:47 +0100

    Update requesting_sw.py

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,7 +1,11 @@
 import requests
 import json
 
+# all the data that I will extract and transform will be contained within 'new'
 new = []
+
+# this function will extract data from every starship in the API. I've hardcoded the number of starships to be extracted (76).
+# the better way would be to use the next page to use for iterations.
 def get_starships(first=0,last=76):
     for uid in range(first,last):
         req = requests.get(f"https://www.swapi.tech/api/starships/{uid}")
@@ -12,7 +16,7 @@ def get_starships(first=0,last=76):
             continue
     return new
 
-
+#this function accesses the data in 'new', enters the links, extracts the objectids, and replaces the links with the objectids.
 def replace_links(first_index=1):
     for num in range(first_index,len(new)):
         for index, item in enumerate(new[num]["properties"]["pilots"]):
