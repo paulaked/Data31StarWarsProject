aff8ec5b37f52e4c38f32c5d2779ea87f61c4111
Benjamin Warren <bwarren@spartaglobal.com>
2022-08-01 15:36:09 +0100

    Upadated README and pulling everything together in main document

--- starwars/main.py
+++ starwars/main.py
@@ -5,15 +5,20 @@ db = client['starwars31']
 db.starships.drop()                                  #drops the starships collection if it already exists.
 new_col = db["starships"]                            #creates a new starships collection
 
+url = "https://swapi.dev/api/starships/?"
+request = requests.get(url)
+json_data = requests.get(url).json()
+no_of_starships = json_data["count"]
 
 def test_length():
-    assert len(get_starships()) == 36               #tests that the code is finding all the requried starships.
+    assert len(get_starships()) == no_of_starships  #tests that the code is finding all the requried starships.
 
 def test_type():
     assert type(get_starships()) == type([])        #test that the function is outputting in the desired format.
 
 
-"""Using Get Request only returned the first 10 results and so I did not use the following code. I have kept it in as if I found out how to change the limit this would have been optimal."""
+"""Using Get Request only returned the first 10 results and so I did not use the following code. I have kept it in as 
+if I found out how to change the limit this would have been optimal."""
 
 # def get_starships():
 #     list_of_starship_urls = []
@@ -29,7 +34,7 @@ def get_starships():                                            # this f will ou
     count_starships = 0                                         # counts how many starships have been located so far.
     i = 1                                                       # starts by looking for a starship in pos 1
     list_of_starships = []                                      # this will be the list containing all the star ship urls
-    while count_starships < 36:                                 # The Star Wars API has 36 starships: https://swapi.dev/about
+    while count_starships < no_of_starships:
         url = "https://swapi.dev/api/starships/" + str(i)
         request = requests.get(url)
         if request.status_code == 200:                          # if the url exists we have found a starship
@@ -39,7 +44,7 @@ def get_starships():                                            # this f will ou
         i = i + 1  # now we test the next available url
     return (list_of_starships)
 
-#Task 2: create a function that takes in a starship and outputs the corrosponding pilot object ID's as a list.
+# Task 2: create a function that takes in a starship and outputs the corrosponding pilot object ID's as a list.
 
 def test_pilot_ID_list():
     test_list = []
@@ -66,15 +71,15 @@ def pilot_ID_list(url):
             pilot_ID_list.append(character_id)  #Adding the object ID of the pilot to a list
     return (pilot_ID_list)
 
-#Task 3 - creates json data of the starships, replacing the pilot ID urls with object IDs of the pilots.
+# Task 3 - creates json data of the starships, replacing the pilot ID urls with object IDs of the pilots.
 
-#This test checks that the output is in the type expected.
+# This test checks that the output is in the type expected.
 
 
 def test_combine_starships_and_pilotID():
     assert type(combine_starships_and_pilotID("https://swapi.dev/api/starships/12",pilot_ID_list("https://swapi.dev/api/starships/12"))) == type({"test": "test"})
 
-#This tests checks that unwnated information has not been included.
+# This tests checks that unwnated information has not been included.
 
 def test_combine_starships_and_pilotID_2():
     assert combine_starships_and_pilotID("https://swapi.dev/api/starships/12",pilot_ID_list("https://swapi.dev/api/starships/12")).get("created") == None
@@ -103,6 +108,19 @@ def test_add_to_collection():
 def add_to_collection(json_data):
     x = new_col.insert_one(json_data)
 
+#Task 5 - Bring all the functions together to create the new collection.
+
+db.starships.drop()                                     # drops the starships collection if it already exists.
+new_col = db["starships"]                               # creates a new collection
+
+for i in range(len(get_starships())):
+    url_of_starship_i = get_starships()[i]
+    list_of_pilot_IDs_for_starship_i = pilot_ID_list(url_of_starship_i)
+    data = combine_starships_and_pilotID(url_of_starship_i,list_of_pilot_IDs_for_starship_i)
+    add_to_collection(data)
+
+    for item in db.starships.find():
+        print(item)
 
 
 
