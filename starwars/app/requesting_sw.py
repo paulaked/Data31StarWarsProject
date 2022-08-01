4294d6cd87674a527b6b44c7bcfdfb13966c0b2a
Hinesh <H-tailor@hotmail.co.uk>
2022-08-01 23:58:43 +0100

    pulled starships data, attempting to pilot list with object ids

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,18 +1,45 @@
-import requests
+import requests     #importing libraries
 import json
+import pymongo
+client = pymongo.MongoClient()  #creating mongodb client
+db = client['starwars31']   #creating reference "db" to database
 
-headers = {'Content-Type':'application/json'}
-starship_req = requests.get("https://swapi.dev/api/starships/",headers=headers)
-
-print(starship_req.json())
+headers = {'Content-Type':'application/json'} #configuration to make the return a json
 
 
-#trying to see pilots list
-resp = starship_req.json()
-pilot_list=[]
-def pilotlist():
-    for i in range(1,36):
-        pilot_list.append(resp["results"][i]["pilots"])
-        return pilot_list
-print(pilotlist())
+def link_to_pilot(pilots): #function to loop through pilots
+    if not pilots:
+        return []   #returning empty array if pilot list is empty
+
+    ids = []
+    for link in pilots: #getting names of pilots
+        character_req = requests.get(link,headers=headers) #requesting pilot characters from api
+        name = character_req.json()["name"] #turn result into json and index by "name"
+        id = db.characters.find({"name":name}, {"name":1, "_id":1})
+        ids.append(id)
+
+    return ids
+
+def starships_with_objectid():
+    starship_req = requests.get("https://swapi.dev/api/starships/",
+                                headers=headers)  # requesting data from api using configuration
+    starships = starship_req.json()["results"]  # turn result into json and index by "results"
+    for starship in starships:
+        #{..., pilots: [], ...},
+        starship["pilots"] = link_to_pilot(starship["pilots"])
+
+    return starships
+print(starships_with_objectid())
+
+
+
+
+# #trying to see pilots list
+# resp = starship_req.json()
+# pilot_list=[]
+# def pilotlist():
+#     for i in range(1,36):
+#         pilot_list.append(resp["results"][i]["pilots"])
+#         return pilot_list
+# print(pilotlist())
 
--- starwars/app/test_requesting_sw.py
+++ starwars/app/test_requesting_sw.py
@@ -8,4 +8,4 @@ json_body = json.dumps(["starships"])
 headers = {'Content-Type':'application/json'}
 starship_req = requests.get("https://swapi.dev/api/starships/",headers=headers, data=json_body)
 
-print(starship_req)
+print(starship_req.json())
