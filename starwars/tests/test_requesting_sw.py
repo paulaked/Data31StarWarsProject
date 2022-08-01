029c29320b460cb4fb785c0125069d938cf2debf
anaghieh <109526313+anaghieh@users.noreply.github.com>
2022-08-01 15:40:35 +0100

    Update test_requesting_sw.py

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,13 @@
 # Unit Testing Plan
 
 # Test function that pulls all available starships from api
+
+import unittest
+import starwars
+
+class TestStarwars(unittest.TestCase):
+
+    def test_get_starships(self):
+        length = len(starwars.get_starships(7,15))
+        result = length
+        self.assertEqual(result, 5)
