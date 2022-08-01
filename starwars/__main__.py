a99390d7c8778f2eaa89a272d435cddf7eb56e60
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-08-02 00:59:54 +0100

    Main file update - added more comments

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -5,12 +5,5 @@ import sys
 
 if __name__ == '__main__':
     db = requesting_sw.create_db_collection('starwars31') # runs the database collection creation function
-    requesting_sw.starship_data(db) # function call from requesting_sw
-    requesting_sw.read_db_collection(db) # function to read the collection
-
-
-
-
-
-
-   # Replace this with code to run your app
+    requesting_sw.starship_data(db) # function call from requesting_sw to extract the data
+    requesting_sw.read_db_collection(db) # function to read and output the collection
