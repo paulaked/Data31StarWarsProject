878f3ae3b8cb91fe038b9fc9ce8a1538bb8028f6
Savan Meggi <you@example.comsmeggi@spartaglobal.comgit config --global user.name Your>
2022-08-01 10:25:15 +0100

    Called back the correct apo

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,1 +1,9 @@
+import requests
+import json
+
+json_body = {"Content-Type": "application/json"}
+
+starships_req = requests.get("http://swapi.dev/api/starships", headers=json_body)
+
+print(starships_req.json())
 
