c6e3e367f71a57358884d09d78bd504582bc8927
Bilal <bilalullah@hotmail.com>
2022-08-01 15:39:13 +0100

    fixed db setup test

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -2,10 +2,22 @@
 
 # Test function that pulls all available starships from api
 
+from starwars.app import requesting_sw
 import unittest
+import sys
+import requests
+import pymongo
 
-# Test function that pulls all available starships from api
 
 class ApiTesting(unittest.TestCase):
 
-    api_test = get_api('https://swapi.dev/')
+    # Test function that pulls data from api.
+    def test_get_from_api(self):
+        status_code = requesting_sw.get_api("https://swapi.dev/api/people").status_code
+        self.assertEqual(status_code, 200, "Incorrect response code")
+
+    # Test Database set up function
+    api_test = requesting_sw.db_link('starwars')
+
+    def test_set_up_db(self):
+        self.assertIsInstance(self.api_test, pymongo.database.Database, "DatabaseX not returned")
