0ad339b61a24af5085013e0605793f5185797cee
Bilal <bilalullah@hotmail.com>
2022-08-01 18:36:19 +0100

    Main file refined

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -10,10 +10,3 @@ if __name__ == '__main__':
     db = requesting_sw.db_link(db_name)
     requesting_sw.tform_in(db)
     requesting_sw.read_db(db)
-    #suppress_text = io.StringIO()
-    #sys.stdout = suppress_text
-    # Run tests
-    #main(module='requesting_sw', exit=False)
-
-    # Release output.
-    #sys.stdout = sys.__stdout__
