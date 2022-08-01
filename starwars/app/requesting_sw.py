b673abc98c9db6821ef7a8e923cc4c9af72517cd
anaghieh <109526313+anaghieh@users.noreply.github.com>
2022-08-01 15:41:17 +0100

    Update requesting_sw.py

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,1 +1,14 @@
+import requests
+import json
 
+new = []
+def get_starships(first=0,last=76):
+
+    for uid in range(first,last):
+        req = requests.get(f"https://www.swapi.tech/api/starships/{uid}")
+        data = req.json()
+        if data["message"] == "ok":
+            new.append(data["result"])
+        else:
+            continue
+    return new
