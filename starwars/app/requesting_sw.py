7c7a54a8acbbf98490050eda080846ccbc4d6198
anaghieh <109526313+anaghieh@users.noreply.github.com>
2022-08-01 21:27:08 +0100

    Update requesting_sw.py

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -11,3 +11,16 @@ def get_starships(first=0,last=76):
         else:
             continue
     return new
+
+
+def replace_links(first_index=1):
+    for num in range(first_index,len(new)):
+        for index, item in enumerate(new[num]["properties"]["pilots"]):
+            req1 = requests.get(item)
+            data1 = req1.json()
+            ids = data1["result"]["_id"]
+            characters = f"ObjectID('{ids}')"
+            count = num
+            ney = new[count]["properties"]["pilots"]
+            ney[index] = characters
+    return new
