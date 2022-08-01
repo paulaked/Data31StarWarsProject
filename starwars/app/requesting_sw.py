72f53483f3bd77a8f964d3f0e38ef9dc58702a4e
gabensaw <gabriel.sawicki1995@gmail.com>
2022-08-01 19:02:15 +0100

    rebuild add pilots_url_to_names to save pilot object ids insteed of names

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -56,12 +56,13 @@ def convert_pilot_url_to_name(url):
 
 def pilots_url_to_names(ships_list):
     for starship in ships_list:
-        pilots_names = []
+        pilots_ids = []
         for pilot in starship.get("pilots"):
             pilot_name = convert_pilot_url_to_name(pilot)
-            # pilot_id = db.characters.find({"name": pilot_name}, {"_id": 1})
-            pilots_names.append(pilot_name)
-        starship["pilots"] = pilots_names
+            pilot_id = db.characters.find({"name": pilot_name}, {"_id": 1})
+            for result in pilot_id:
+                pilots_ids.append(result["_id"])
+        starship["pilots"] = pilots_ids
     return ships_list
 
 
@@ -75,16 +76,18 @@ def add_to_starships_coll(documents):
 url = "http://swapi.dev/api/starships"
 
 db = connect_with_db()
-# db.starships.drop()
+db.starships.drop()
 
 connection_dict = api_connection(url)
 ships_list = get_all_starships(connection_dict)
 clean_ships_list = clean_all_starships(ships_list)
 
+# add to collection
 clean_ships_with_names = pilots_url_to_names(clean_ships_list)
 add_to_starships_coll(clean_ships_with_names)
 
+### check db
 
-
-#### neew
-# add_data = add_to_collection(pilots_names_new)
+ships = db.starships.find({})
+for num, s in enumerate(ships):
+    print(num+1, f"{s['name']} : {s['pilots']}")
