b92f4ff8b5dd7ad3a311652836c7124099bcb4ed
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-08-02 00:20:27 +0100

    __main__.py file, main file to run the etl process

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -1,3 +1,16 @@
 
+from app import requesting_sw
+import sys
+
+
 if __name__ == '__main__':
-    pass  # Replace this with code to run your app
+    db = requesting_sw.create_db_collection('starwars31') # runs the database collection creation function
+    requesting_sw.starship_data(db) # function call from requesting_sw
+    requesting_sw.read_db_collection(db) # function to read the collection
+
+
+
+
+
+
+   # Replace this with code to run your app
