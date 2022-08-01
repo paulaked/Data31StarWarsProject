2c142041137d2e576f0c676aed4219ccd837c0e8
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-08-01 10:52:32 +0100

    Test function to check starship count and semi complete test to check if database exists

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -3,17 +3,38 @@
 import unittest
 import sys
 
+import pymongo.database
+import requests
+
 sys.path.insert(0,"..")
 from ..app.requesting_sw import *
 
 
 class APIUnittests(unittest.TestCase):
 
-    api_testing = GetStarwarsAPI()
 
     def test_get_api(self):
         '''
         Check to see if API is responding
-        :return: checks if the status code matches the provided value
+        :return: Passed or Failed (checks if the status code matches the provided value)
         '''
-        self.assertEqual(self.api_testing.get_data_api().status_code, 200)
+        response = requests.get("https://swapi.dev/api/starships/")
+        assert response.status_code == 200
+
+    def test_starship_count(self):
+        '''
+        Test to check the number of starships
+        :return: Passed or Failed (if value matches)
+        '''
+        response = requests.get("https://swapi.dev/api/starships/")
+        response_body = response.json()
+        assert response_body["count"] == 36 #value of number of starships
+
+
+   # db = requesting_sw['starwars31']
+   # def test_for_db(self):
+   #     '''
+   #     Test to check if the database created exists
+   #     :return: Passed or Failed (based on if database is found)
+   #     '''
+   #     self.assertEqual(self.db, pymongo.database.Database())
