5ee9d8eb621a896e12e899377a38fdc4f0e05a84
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-08-01 12:46:10 +0100

    Function for creating a collection in database (adapted with error handling)

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -13,6 +13,25 @@ class GetStarwarsAPI:
         status_code = requests.get("https://swapi.dev/")
         return status_code
 
+    def create_db_collection(name_db):
+        '''
+        Function to create a database collection
+        :return:
+        '''
+
+        client = pymongo.MongoClient() # connect to database
+        db = client["starwars31"] #database client 'starwars31'
+
+
+        try:
+            db.create_collection("starships") # function tries to checks if the collection already pre-exists
+        except:
+            print("Collection 'starships' already exists.. current collection will be deleted...") #if collection pre-exists
+            db.starships.drop() #current collection dropped
+            db.create_collection("starships") #new collected created
+            print("New 'starships' Collection is created..")
+        return db #return updated db
+
 
 
 
