f5cd08b5d56e608258d47aaa813b8fa80a724820
sanjay-spartaglobal <109526369+sanjay-spartaglobal@users.noreply.github.com>
2022-08-02 08:36:48 +0100

    Update test_requesting_sw.py
    completed first test to check the request

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -1,3 +1,8 @@
 # Unit Testing Plan
+import requests
+from app.requesting_sw import *
 
-# Test function that pulls all available starships from api
+# the 200 status code means the requests works
+def check_api_request():
+    check_starwars_api = requests.get("https://swapi.dev/api/")
+    assert check_starwars_api.status_code == 200
