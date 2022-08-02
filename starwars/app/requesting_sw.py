da591215d0db991cca86f05daf6d8e4d2bd97c96
sanjay-spartaglobal <109526369+sanjay-spartaglobal@users.noreply.github.com>
2022-08-02 08:07:07 +0100

    Update requesting_sw.py
    added drop starships function

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,1 +1,13 @@
+import pymongo
+import requests
 
+client = pymongo.MongoClient()
+db = client["starwars31"]
+
+
+# drop existing starships collections
+def drop_starships():
+    if db.starships.find({}) != "":
+        db.starships.delete_many({})
+        # drops the entire collection
+    db.starships.drop()
