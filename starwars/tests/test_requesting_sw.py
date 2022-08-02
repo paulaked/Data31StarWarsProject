856a5cf94ef6c242816cf21af826f24146721c83
sanjay-spartaglobal <109526369+sanjay-spartaglobal@users.noreply.github.com>
2022-08-02 08:37:53 +0100

    Update test_requesting_sw.py
    checking order_id's a string

--- starwars/tests/test_requesting_sw.py
+++ starwars/tests/test_requesting_sw.py
@@ -6,3 +6,8 @@ from app.requesting_sw import *
 def check_api_request():
     check_starwars_api = requests.get("https://swapi.dev/api/")
     assert check_starwars_api.status_code == 200
+
+# checking order_id's is a string
+def test_get_from_order_ids():
+    for i in replace_order_ids():
+        assert type(i['name']) is str    
